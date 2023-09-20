15a425fecd88669b30cac094743a8a0177ef7016
Jacek Witkiewicz <jacek.witkiewicz@gmail.com>
2023-09-20 18:06:16 +0200

    TarGET: main -> cmd

--- main.go
+++ main.go
@@ -1,15 +1,16 @@
 package main
 
-import (
-	"github.com/practical-coder/target/release"
-)
+import "github.com/practical-coder/target/cmd"
 
 func main() {
-	r := release.NewRelease(
-		"https://api.github.com/repos/dominikh/go-tools/releases/latest",
-		"staticcheck_linux_amd64.tar.gz",
-	)
-	r.Setup()
+	cmd.Execute()
+	/*
+		r := release.NewRelease(
+			"https://api.github.com/repos/dominikh/go-tools/releases/latest",
+			"staticcheck_linux_amd64.tar.gz",
+		)
+		r.Setup()
+	*/
 	// tarURL := r.TarURL()
 	// Get ReleaseUrl
 	// Grep browser_download_url with TarName
