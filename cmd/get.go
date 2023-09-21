84ff3ac9f5fff62bbb9e3f53409f3b4729192ce3
Jacek Witkiewicz <jacek.witkiewicz@gmail.com>
2023-09-21 08:29:47 +0200

    Release download: URL and asset file configurable from CLI flags

--- cmd/get.go
+++ cmd/get.go
@@ -4,17 +4,28 @@ import (
 	"fmt"
 
 	"github.com/practical-coder/target/release"
+	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 )
+
+func init() {
+	getCmd.Flags().String("file", "staticcheck_linux_amd64.tar.gz", "Asset file name to download")
+	getCmd.Flags().String("url", "https://api.github.com/repos/dominikh/go-tools/releases/latest", "Release URL")
+}
 
 var getCmd = &cobra.Command{
 	Use:   "get",
 	Short: "GET release file",
 	Run: func(cmd *cobra.Command, args []string) {
-		r := release.NewRelease(
-			"https://api.github.com/repos/dominikh/go-tools/releases/latest",
-			"staticcheck_linux_amd64.tar.gz",
-		)
+		file, err := cmd.Flags().GetString("file")
+		if err != nil {
+			log.Fatal().Err(err).Msg("Asset filename missing")
+		}
+		url, err := cmd.Flags().GetString("url")
+		if err != nil {
+			log.Fatal().Err(err).Msg("Release URL missing")
+		}
+		r := release.NewRelease(url, file)
 		r.Setup()
 		fmt.Println(r.TarURL)
 	},
